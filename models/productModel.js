6ce92155e1c23805eb21a2cab574e83643fbe2e4
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2023-03-22 10:42:55 +0100

    update

--- models/productModel.js
+++ models/productModel.js
@@ -90,7 +90,8 @@ productSchema.virtual('reviews', {
 productSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'category',
-    select: 'name -_id' // pour enlever l'ID en met -_id
+    // select: 'name -_id' // pour enlever l'ID en met -_id
+    select: 'name _id' // pour enlever l'ID en met -_id
   });
   next();
 });
