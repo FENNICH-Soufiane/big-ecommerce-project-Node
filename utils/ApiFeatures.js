fb81f974ff99e25eb6791cbe519f4e602f380948
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2023-03-25 11:57:45 +0000

    update

--- utils/ApiFeatures.js
+++ utils/ApiFeatures.js
@@ -1,5 +1,121 @@
-class ApiFeatures {
+// class ApiFeatures {
 
+//   constructor(mongooseQuery, queryString) {
+//     this.mongooseQuery = mongooseQuery;
+//     this.queryString = queryString;
+//   }
+
+//   filter() {
+//     const queryStringObj = { ...this.queryString };
+//     const excludesFields = ['page', 'sort', 'limit', 'fields'];
+//     excludesFields.forEach(field => delete queryStringObj[field]);
+
+//     let queryStr = JSON.stringify(queryStringObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+
+//     this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr))
+
+//     return this;
+//   }
+
+//   sort() {
+//     if (this.queryString.sort) {
+//       const sortBy = this.queryString.sort.split(',').join(' ');
+//       this.mongooseQuery = this.mongooseQuery.sort(sortBy)
+//     } else {
+//       this.mongooseQuery = this.mongooseQuery.sort('-createAt')
+//     }
+//     return this;
+//   }
+
+//   limitFields() {
+//     if (this.queryString.fields) {
+//       const fields = this.queryString.fields.split(',').join(' ');
+//       this.mongooseQuery = this.mongooseQuery.select(fields);
+//     }
+//     else {
+//       // exclude field __v
+//       this.mongooseQuery = this.mongooseQuery.select('-__v')
+//     }
+//     return this;
+//   }
+
+//   search(modelName) {
+//     if (this.queryString.keyword) {
+//       let query = {};
+//       if(modelName === 'Product') {
+//         query.$or = [
+//           { title: { $regex: this.queryString.keyword, $options: 'i' } },
+//           { description: { $regex: this.queryString.keyword, $options: 'i' } },
+//         ];
+//       } else {
+//         query = { name: { $regex: this.queryString.keyword, $options: 'i' } }
+//       }
+
+//       this.mongooseQuery = this.mongooseQuery.find(query);
+//     }
+//     return this;
+//   }
+
+//   // paginate(countDocuments) {
+//   //   const page = this.queryString.page * 1 || 1;
+//   //   const limit = this.queryString.limit * 1 || 20;
+//   //   // const limit = this.queryString.limit * 1;
+//   //   const skip = (page - 1) * limit;
+//   //   const endIndex = page * limit;
+    
+//   //   //  Pagination result
+//   //   const pagination = {};
+//   //   pagination.currentPage = page;
+//   //   pagination.limit = limit;
+//   //   pagination.numberOfPages = Math.ceil(countDocuments / limit);
+
+//   //   // next page
+//   //   if (endIndex < countDocuments) {
+//   //     pagination.next = page + 1;
+//   //   }
+//   //   if (skip > 0) {
+//   //     pagination.prev = page - 1;
+//   //   }
+
+//   //   this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+
+//   //   this.paginationResult = pagination;
+//   //   return this;
+
+
+//   // }
+//   paginate(countDocuments) {
+//     const page = this.queryString.page * 1 || 1;
+//     const limit = this.queryString.limit * 1 || 50;
+//     const skip = (page - 1) * limit;
+//     const endIndex = page * limit;
+
+//     // Pagination result
+//     const pagination = {};
+//     pagination.currentPage = page;
+//     pagination.limit = limit;
+//     pagination.numberOfPages = Math.ceil(countDocuments / limit);
+
+//     // next page
+//     if (endIndex < countDocuments) {
+//       pagination.next = page + 1;
+//     }
+//     if (skip > 0) {
+//       pagination.prev = page - 1;
+//     }
+//     this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+
+//     this.paginationResult = pagination;
+//     return this;
+//   }
+// }
+
+
+
+// module.exports = ApiFeatures;
+
+class ApiFeatures {
   constructor(mongooseQuery, queryString) {
     this.mongooseQuery = mongooseQuery;
     this.queryString = queryString;
@@ -8,12 +124,12 @@ class ApiFeatures {
   filter() {
     const queryStringObj = { ...this.queryString };
     const excludesFields = ['page', 'sort', 'limit', 'fields'];
-    excludesFields.forEach(field => delete queryStringObj[field]);
-
+    excludesFields.forEach((field) => delete queryStringObj[field]);
+    // Apply filtration using [gte, gt, lte, lt]
     let queryStr = JSON.stringify(queryStringObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr))
+    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr));
 
     return this;
   }
@@ -21,9 +137,9 @@ class ApiFeatures {
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
-      this.mongooseQuery = this.mongooseQuery.sort(sortBy)
+      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
     } else {
-      this.mongooseQuery = this.mongooseQuery.sort('-createAt')
+      this.mongooseQuery = this.mongooseQuery.sort('-createAt');
     }
     return this;
   }
@@ -32,10 +148,8 @@ class ApiFeatures {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
       this.mongooseQuery = this.mongooseQuery.select(fields);
-    }
-    else {
-      // exclude field __v
-      this.mongooseQuery = this.mongooseQuery.select('-__v')
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select('-__v');
     }
     return this;
   }
@@ -43,13 +157,13 @@ class ApiFeatures {
   search(modelName) {
     if (this.queryString.keyword) {
       let query = {};
-      if(modelName === 'Product') {
+      if (modelName === 'Products') {
         query.$or = [
           { title: { $regex: this.queryString.keyword, $options: 'i' } },
           { description: { $regex: this.queryString.keyword, $options: 'i' } },
         ];
-      } else {
-        query = { name: { $regex: this.queryString.keyword, $options: 'i' } }
+      } else  {
+        query = { name: { $regex: this.queryString.keyword, $options: 'i' } };
       }
 
       this.mongooseQuery = this.mongooseQuery.find(query);
@@ -57,34 +171,6 @@ class ApiFeatures {
     return this;
   }
 
-  // paginate(countDocuments) {
-  //   const page = this.queryString.page * 1 || 1;
-  //   const limit = this.queryString.limit * 1 || 20;
-  //   // const limit = this.queryString.limit * 1;
-  //   const skip = (page - 1) * limit;
-  //   const endIndex = page * limit;
-    
-  //   //  Pagination result
-  //   const pagination = {};
-  //   pagination.currentPage = page;
-  //   pagination.limit = limit;
-  //   pagination.numberOfPages = Math.ceil(countDocuments / limit);
-
-  //   // next page
-  //   if (endIndex < countDocuments) {
-  //     pagination.next = page + 1;
-  //   }
-  //   if (skip > 0) {
-  //     pagination.prev = page - 1;
-  //   }
-
-  //   this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
-
-  //   this.paginationResult = pagination;
-  //   return this;
-
-
-  // }
   paginate(countDocuments) {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 50;
@@ -110,7 +196,5 @@ class ApiFeatures {
     return this;
   }
 }
-
-
 
 module.exports = ApiFeatures;
